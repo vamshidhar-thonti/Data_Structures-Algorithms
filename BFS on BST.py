b0febe417dd2341e9d3da8260facbaa3ac5dc6a9
vamshidhar281997 <t.vamshidhar333@gmail.com>
2022-01-02 21:30:48 +0530

    more stuff

---
+++ BFS on BST.py
@@ -0,0 +1,108 @@
+import sys, json
+
+def traverse(node):
+  tree = { 'value': node.value }
+  tree['left'] = None if node.left == None else traverse(node.left)
+  tree['right'] = None if node.right == None else traverse(node.right)
+  return tree
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, value):
+        newNode = Node(value)
+        isPositionFound = False
+        start = self.root
+        if self.root == None:
+            self.root = newNode
+        else:
+            while not isPositionFound:
+                if value > start.value:
+                    if start.right == None:
+                        start.right = newNode
+                        isPositionFound = True
+                    else:
+                        start = start.right
+                elif value < start.value:
+                    if start.left == None:
+                        start.left = newNode
+                        isPositionFound = True
+                    else:
+                        start = start.left
+                else:
+                    sys.exit("value should be either > or <")
+
+
+    def lookup(self, value):
+        start = self.root
+        while start is not None:
+            if value == start.value:
+                return start
+            elif value < start.value:
+                start = start.left
+            else:
+                start = start.right
+        
+        return False
+
+    def remove(self, value):
+        pass
+        # Need to work on it
+
+    def breadthfirstSearch(self):
+        currentNode = self.root
+        bfs_list = []
+        queue = [currentNode]
+
+        while len(queue) > 0:
+            currentNode = queue.pop(0)
+            bfs_list.append(currentNode.value)
+
+            if currentNode.left is not None:
+                queue.append(currentNode.left)
+            if currentNode.right is not None:
+                queue.append(currentNode.right)
+
+        return bfs_list
+
+    def breadthFirstSearchRecursive(self, queue, bfs_list):
+        if len(queue) == 0:
+            return bfs_list
+        
+        currentNode = queue.pop(0)
+        bfs_list.append(currentNode.value)
+        if currentNode.left is not None:
+            queue.append(currentNode.left)
+        if currentNode.right is not None:
+            queue.append(currentNode.right)
+        
+        return self.breadthFirstSearchRecursive(queue, bfs_list)
+
+bst = BinarySearchTree()
+bst.insert(100)
+bst.insert(74)
+bst.insert(32)
+bst.insert(120)
+bst.insert(86)
+bst.insert(111)
+bst.insert(150)
+# print(vars(bst.lookup(150)))
+# print(vars(bst.lookup(100)))
+# print(vars(bst.lookup(32)))
+# print(vars(bst.lookup(74)))
+# print(vars(bst.lookup(86)))
+# print(vars(bst.lookup(111)))
+# print(vars(bst.lookup(120)))
+# print(vars(bst.lookup(1)))
+
+print(bst.breadthfirstSearch())
+print(bst.breadthFirstSearchRecursive([bst.root], []))
+
+# print(json.dumps(traverse(bst.root), indent=4, sort_keys=True))
---
+++ Trie.py
@@ -0,0 +1,79 @@
+class Node:
+    
+    def __init__(self, value) -> None:
+        self.value = value
+        self.children = {}
+        self.end_here = False
+
+class Trie:
+
+    def __init__(self) -> None:
+        self.root = Node(None)
+
+    def insert(self, word):
+        parent = self.root
+        for index, char in enumerate(word):
+            if char not in parent.children:
+                parent.children[char] = Node(word)
+            parent = parent.children[char]
+            if index == len(word) - 1:
+                parent.end_here = True
+
+    def search(self, word):
+        parent = self.root
+        for char in word:
+            if char not in parent.children.keys():
+                return False
+            parent = parent.children[char]
+        return parent.end_here
+
+    def starts_with(self, sub_word):
+        parent = self.root
+        for char in sub_word:
+            if char not in parent.children.keys():
+                return False
+            parent = parent.children[char]
+        return True
+
+    def delete(self, word):
+        '''
+            Doesn't delete the sub nodes if no children.
+            It will just set the end_here value to False (only if that value is already inserted),
+            so that it doesn't be found where search method is called.
+            But if starts_with method is called, it returns True.
+        '''
+        parent = self.root
+        for char in word:
+            if char not in parent.children.keys():
+                return "No such word exist"
+            if char in parent.children.keys():
+                parent = parent.children[char]
+                if parent.end_here:
+                    parent.end_here = False
+                    return f"Deleted {word}"
+trie = Trie()
+
+trie.insert('apple')
+trie.insert('apples')
+trie.insert('approx')
+trie.insert('bad')
+trie.insert('ban')
+trie.insert('bank')
+trie.insert('band')
+
+print(trie.search('apple'))
+print(trie.search('apples'))
+print(trie.starts_with('app'))
+
+print(trie.search('bad'))
+print(trie.search('ban'))
+print(trie.search('bank'))
+print(trie.search('band'))
+
+print(trie.delete('ban'))
+print(trie.search('bad'))
+print(trie.search('ban'))
+print(trie.search('bank'))
+print(trie.search('band'))
+
+print(trie.starts_with('ban'))
